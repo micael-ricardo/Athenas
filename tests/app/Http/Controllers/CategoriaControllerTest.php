359557f292f29bda00749b22e982353fbac1bc01
micael-ricardo <micael.ricardo@outlook.com>
2023-08-02 17:49:32 -0300

    Criar Seedrs para popular o banco de dados

---
+++ tests/app/Http/Controllers/CategoriaControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\Http\Controllers;
+
+use Tests\TestCase;
+
+class CategoriaControllerTest extends TestCase
+{
+    public function InsertCategoria()
+    {
+        
+        
+    }
+}
