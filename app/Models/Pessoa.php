c6f4713f578d7524dfac160aad6a59e2837409df
micael-ricardo <micael.ricardo@outlook.com>
2023-08-02 14:46:54 -0300

    Criar Migrates + Model

---
+++ app/Models/Pessoa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pessoa extends Model
+{
+    protected $fillable = ['nome', 'cpf', 'email', 'categoria_id'];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+}
